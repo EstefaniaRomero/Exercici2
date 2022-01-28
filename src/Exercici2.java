38e9159e2c5e5fb222e67b8d36071dc0271dc6e3
EstefaniaRomero <estefaniaromero.torrents@gmail.com>
2022-01-28 12:29:22 +0100

    Initial commit

---
+++ src/Exercici2.java
@@ -0,0 +1,16 @@
+public class Exercici2 {
+
+    public static void main(String[] args){
+
+
+        double a = 2.3;
+        double b = 5.5;
+        double c = 3.2;
+
+        System.out.println("Resultado " + (a +b) / (c) );
+
+
+
+    }
+
+}
